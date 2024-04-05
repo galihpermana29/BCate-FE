5d4ed2f7fb262945d9c768dfddbd1618bff2cafc
Galih Permana <101629052+galihpermana292@users.noreply.github.com>
2024-04-05 22:10:46 +0700

    fixing download blueprint

--- components/chat/room-chat.tsx
+++ components/chat/room-chat.tsx
@@ -1,8 +1,11 @@
 import { ArrowLeftOutlined } from "@ant-design/icons"
-import { Divider, Form, FormInstance, Input } from "antd"
+import { Divider, Form, FormInstance, Input, message } from "antd"
+import { DesignAPI } from "api/designService"
+import { Design } from "api/response-interface"
 import BoxChat from "components/chat/box-chat"
 import { db } from "firebase-config"
 import { doc, onSnapshot } from "firebase/firestore"
+import useAuth from "hooks/useAuth"
 import { useEffect, useState } from "react"
 
 interface RoomChatI {
@@ -15,6 +18,9 @@ interface RoomChatI {
 const RoomChat = ({ handleSubmitText, form, activeRoom }: RoomChatI) => {
   const [messages, setMessages] = useState<any>({ messages: [] })
 
+  const { authData } = useAuth()
+  const [revisions, setRevisions] = useState<string[] | null>(null)
+
   useEffect(() => {
     const unSub = onSnapshot(doc(db, "chats", activeRoom.roomId), (doc) => {
       doc.exists() && setMessages(doc.data())
@@ -24,6 +30,23 @@ const RoomChat = ({ handleSubmitText, form, activeRoom }: RoomChatI) => {
       unSub()
     }
   }, [activeRoom.roomId])
+
+  useEffect(() => {
+    const getDesignDetail = async () => {
+      try {
+        const res = await DesignAPI.getById(activeRoom.roomId.split("")[2] as string, authData?.token!)
+        const transactions = res.data.transactions
+        transactions.forEach(({ user, revision }) => {
+          if (user.id === authData?.user.id && revision) setRevisions(revision)
+        })
+      } catch (e) {
+        message.error("Error while getting the data")
+      }
+    }
+
+    if (authData && activeRoom.roomId) getDesignDetail()
+  }, [activeRoom.roomId])
+
   return (
     <div className="relative h-[86vh] w-full px-[29px] py-[24px]">
       <div className="absolute inset-x-[29px] top-[24px] z-[99] bg-white">
@@ -42,7 +65,19 @@ const RoomChat = ({ handleSubmitText, form, activeRoom }: RoomChatI) => {
         <BoxChat chat={messages.messages} />
       </div>
 
-      <div>
+      <div className="absolute inset-x-[29px] bottom-[0]">
+        {revisions && (
+          <div className="absolute bottom-[75px] flex h-[80px] w-full items-center justify-center rounded-md bg-black">
+            <div className="flex flex-1 items-center justify-between px-[20px]">
+              <div className="text-white">Get your blueprints</div>
+              <a href={revisions[0]} download={"blueprint.jpg"} target="_blank">
+                <button type="submit" className="rounded bg-[#FFF] px-[16px] py-[8px] text-black">
+                  Download
+                </button>
+              </a>
+            </div>
+          </div>
+        )}
         <Form
           form={form}
           onFinish={handleSubmitText}
